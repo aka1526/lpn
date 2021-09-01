80bd29d2404bc7afae1f6217b3b0ebf6cb068d57
Akachai <akachai1526@hotmail.com>
2021-09-01 18:00:28 +0700

    edit

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -1,24 +1,28 @@
 <?php
 namespace App\Http\Controllers\Frontend;
 use App\Http\Controllers\Controller;
+
 use Illuminate\Http\Request;
-use DB;
+use Illuminate\Support\Str;
 use Carbon\Carbon;
+use Cookie;
+use File;
+use DB;
 
 use App\Models\Admins\Slidepage;
 
+
 class HomeController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
+    protected $paging = 10;
+    protected $useruid = '';
+
     public function home()
     {
-        $slidepage =Slidepage::where('slidepages_status','=','Y')
-        ->orderBy('slidepages_index')->get();
-        return view('frontend.pages.home',compact('slidepage'));
+        $slidepage = Slidepage::where('slidepages_status', '=', 'Y')
+        ->orderBy('slidepages_index')->paginate($this->paging);
+         return view('frontend.pages.home',compact('slidepage'));
     }
 
     /**
--- app/Models/Admins/Slidepage.php
+++ app/Models/Admins/Slidepage.php
@@ -15,17 +15,16 @@ class Slidepage extends Model
     public $table = 'slidepages';
 
     protected $fillable = [
-        'slidepages_uid' ,
-            'slidepages_index' ,
-            'slidepages_headline' ,
-            'slidepages_header' ,
-            'slidepages_detail' ,
-            'slidepages_link' ,
-            'slidepages_img' ,
-            'slidepages_status' ,
-            'created_at' ,
-            'update_at',
-    
+        'slidepages_uid',
+        'slidepages_index',
+        'slidepages_headline',
+        'slidepages_header',
+        'slidepages_detail',
+        'slidepages_link',
+        'slidepages_img',
+        'slidepages_status',
+        'created_at',
+        'update_at',
+
     ];
 }
- 
