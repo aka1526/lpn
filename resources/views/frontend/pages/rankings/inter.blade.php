942deddae355febdafb8bb034c6e9ebe63d784f3
Akachai <akachai1526@hotmail.com>
2021-10-06 18:09:20 +0700

    edit

--- resources/views/frontend/pages/rankings/inter.blade.php
+++ resources/views/frontend/pages/rankings/inter.blade.php
@@ -47,13 +47,13 @@
                 <h2 class="text-center text-danger">{{ isset($rankingslist[0]) ? $rankingslist[0]->contenders_gander : '' }} CHAMPIONS</h2>
                 
 
-                <div class="row row-sm mt-4">
+                <div class="mt-4 row row-sm">
                     @if (isset($rankingslist))
                     @foreach ($rankingslist as $item)
                     
-                    <div class="col-md-3 col-lg-3">
+                    <div class="col-md-4 col-lg-4">
                         <div class="card">
-                            <div class="card-header tx-medium bd-0 tx-primary text-center">
+                            <div class="text-center card-header tx-medium bd-0 tx-primary">
                                 <h5 class="card-title tx-dark tx-medium mg-b-10" style="color: #000000!important;font-size: 20px;">{{ $item->rankings_weight }}</h5>
                                 <p class="card-subtitle mg-b-15" style="color: #51b788!important;font-size: 16px;">{{ $item->rankings_weight_desc }}</p>
                             </div>
@@ -65,10 +65,10 @@
                           
                                
                             </div>
-                            <div class="card-text bd-t text-center">
-                                <h4 class="card-title mb-3 text-center" style="color: #000000!important;font-size: 20px;">{{ $item->contenders }}</h4>
-                                <p class="card-text text-center"> Won Title : <strong>{{ $item->won_title }}</strong> </p>
-                                <p class="card-text text-center"> Last Defense : <strong>{{ $item->last_defense }}</strong> </p>
+                            <div class="text-center card-text bd-t">
+                                <h4 class="mb-3 text-center card-title" style="color: #000000!important;font-size: 20px;">{{ $item->contenders }}</h4>
+                                <p class="text-center card-text"> Won Title : <strong>{{ $item->won_title }}</strong> </p>
+                                <p class="text-center card-text"> Last Defense : <strong>{{ $item->last_defense }}</strong> </p>
                             </div>
                         </div>
                     </div><!-- col-4 -->
