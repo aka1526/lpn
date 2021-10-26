156d96967be43844bea88f7c7a3b7845ba7a95dd
Akachai <akachai1526@hotmail.com>
2021-10-26 19:06:52 +0700

    gallry

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,9 +1,27 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Page Not Found</title>
-</head>
-<body>
-This is the custom 404 error page.
-</body>
-</html>
+@extends('admins.layout.index_main')
+@section('title')
+    Admin Page
+@endsection
+ 
+
+@section('content')
+ <!-- Start of not found content section
+         ============================================= -->
+         <section id="not-found" class="not-found-section">
+            <div class="container">
+               <div class="not-found-content text-center">
+                  <span>Opps! things you are looking for doesn’t exists!</span>
+                  <a href="#">Return Home <i class="flaticon-arrow"></i></a>
+                  <div class="error-img">
+                     <img src="assets/img/er.png" alt="">
+                  </div>
+               </div>
+            </div>
+         </section>
+      <!-- End of not found content section
+         ============================================= -->   
+
+
+
+@endsection
+  
