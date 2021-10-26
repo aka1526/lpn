bf396196f0b20cf4cf0ed3132ba6fe8d149e508e
Akachai <akachai1526@hotmail.com>
2021-10-26 10:54:51 +0700

    gallry

--- resources/views/frontend/includes/header.blade.php
+++ resources/views/frontend/includes/header.blade.php
@@ -19,9 +19,9 @@
 <link rel="shortcut icon" href="{{ asset('/assets/img/icon/web/favicon.ico') }}" type="image/x-icon">
 <link rel="icon" href="{{ asset('/assets/img/icon/web/favicon.ico') }}" type="image/x-icon">
 
-<meta property="og:image" content="{{ asset('/assets/img/socialimg/bg11.jpg') }}">
+{{-- <meta property="og:image" content="{{ asset('/assets/img/socialimg/bg11.jpg') }}">
 <meta property="og:url" content="{{ URL::to('/') }}">
-<meta property="og:type" content="website">
+<meta property="og:type" content="website"> --}}
 <meta property="og:title"
     content="หน้าแรก | สนามมวยลุมพีนี  | โรงเรียนสอนศิลปะการต่อสู้ | โรงเรียนมวยไทยลุมพิน | Lumpinee academy muaythai">
 <meta property="og:description"
